b22daf719cfd1a1efcf4abba13d3775baa8c34c2
Alexey Subbotin <alesu1543@gmail.com>
2023-04-07 18:17:19 +0300

    snd

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 // католог с модулем для синхр. работы с MySQL, который должен быть усталовлен командой: sync-mysql
 const dir_nms = 'C:\\nodejs\\node_modules\\sync-mysql';
+
+// работа с базой данных.
+const Mysql = require(dir_nms)
+const connection = new Mysql({
+    host:'localhost', 
+    user:'root', 
+    password:'', 
+    database:'test'
+})
+
+// обработка параметров из формы.
+var qs = require('querystring');
+function reqPost (request, response) {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+        });
+
+        request.on('end', function () {
+			var post = qs.parse(body);
+			var sInsert = "INSERT INTO myarttable (text, description, keywords) VALUES (\""+post['col1']+"\",\""+post['col2']+"\",\""+post['col3']+"\")";
+			var results = connection.query(sInsert);
+            console.log('Done. Hint: '+sInsert);
+        });
+    }
+}
+
+// выгрузка массива данных.
+function ViewSelect(res) {
+	var results = connection.query('SHOW COLUMNS FROM myarttable');
+	res.write('<tr>');
+	for(let i=0; i < results.length; i++)
+		res.write('<td>'+results[i].Field+'</td>');
+	res.write('</tr>');
+
+	var results = connection.query('SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC');
+	for(let i=0; i < results.length; i++)
+		res.write('<tr><td>'+String(results[i].id)+'</td><td>'+results[i].text+'</td><td>'+results[i].description+'</td><td>'+results[i].keywords+'</td></tr>');
+}
+function ViewVer(res) {
+	var results = connection.query('SELECT VERSION() AS ver');
+	res.write(results[0].ver);
+}
+
 // создание ответа в браузер, на случай подключения.
 const http = require('http');
 const server = http.createServer((req, res) => {
+	reqPost(req, res);
+	console.log('Loading...');
+	
 	res.statusCode = 200;
 //	res.setHeader('Content-Type', 'text/plain');
 
@@ -19,40 +68,6 @@ const server = http.createServer((req, res) => {
 	res.end();
 	console.log('1 User Done.');
 });
-
-// работа с базой данных.
-const Mysql = require(dir_nms)
-const connection = new Mysql({
-    host:'localhost', 
-    user:'root', 
-    password:'', 
-    database:'test'
-})
-
-/*
-const express = require("express");
-const app = express();
-app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
- //   response.send(`${request.body.userName} - ${request.body.userAge}`);
-});
-*/
-function ViewSelect(res) {
-	var results = connection.query('SHOW COLUMNS FROM myarttable');
-	res.write('<tr>');
-	for(let i=0; i < results.length; i++)
-		res.write('<td>'+results[i].Field+'</td>');
-	res.write('</tr>');
-
-	var results = connection.query('SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC');
-	for(let i=0; i < results.length; i++)
-		res.write('<tr><td>'+String(results[i].id)+'</td><td>'+results[i].text+'</td><td>'+results[i].description+'</td><td>'+results[i].keywords+'</td></tr>');
-}
-function ViewVer(res) {
-	var results = connection.query('SELECT VERSION() AS ver');
-	res.write(results[0].ver);
-}
 
 // запуск сервера, ожидание подключений из браузера.
 const hostname = '127.0.0.1';
